03145d690365570bfacc7f8dd82c815393a2979d
armandnjapou <armand.njapoungassu@consulting-for.accor.com>
2024-12-08 15:37:05 +0100

    Init repo

---
+++ src/test/java/com/cwa/readmegenerator/ReadmeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwa.readmegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadmeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
