b82fbe135cfadb493d6182277db31aa598e734cf
armandnjapou <armand.njapoungassu@consulting-for.accor.com>
2024-12-08 14:38:37 +0100

    Init repo

---
+++ src/main/java/com/cwa/documentationgenerator/DocumentationGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.cwa.documentationgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentationGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentationGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cwa/documentationgenerator/DocumentationGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwa.documentationgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentationGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
